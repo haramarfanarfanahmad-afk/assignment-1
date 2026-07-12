4e4e06aa6fbfecfa65c5afd22aecb5a106a66eae
shaguftamemon563-sketch <shaguftamemon563@gmail.com>
2026-07-12 09:50:04 -0700

    Nested if else task 7, 8, and 9 are added

---
+++ nested if-else statement 9.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+
+    cout<<"Enter a Number: ";
+    cin>>num;
+
+    if(num >= 0)
+    {
+        if(num < 100)
+        {
+            cout<<"Number is less than 100";
+        }
+        else
+        {
+            if(num == 100)
+            {
+                cout<<"Number is equal to 100";
+            }
+            else
+            {
+                cout<<"Number is greater than 100";
+            }
+        }
+    }
+    else
+    {
+        cout<<"Invalid Number";
+    }
+
+    return 0;
+}
