c7e683d591dd7cd75ad75c1f589b6e4969449170
haramarfanarfanahmad-afk <haramarfanarfanahmad@gmail.com>
2026-07-13 17:49:28 +0500

    Add files via upload

---
+++ haram/p07_if.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int seats;
+
+    cout << "Enter Available Seats: ";
+    cin >> seats;
+
+    if(seats > 0)
+    {
+        cout << "Seats Available";
+    }
+
+    return 0;
+}
