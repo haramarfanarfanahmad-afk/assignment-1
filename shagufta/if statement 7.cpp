eccabf34eb1a11b7b7ecd9b9844835bee499b1b7
shaguftamemon563-sketch <shaguftamemon563@gmail.com>
2026-07-13 07:23:34 -0700

    Add files via upload

---
+++ shagufta/if statement 7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char ch;
+	cout<<"Enter a Character: ";
+	cin>>ch;
+	if(ch=='A')
+	{
+		cout<<"Character is A";
+	}
+	return 0;
+}
---
+++ shagufta/if statement 8.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int salary;
+	cout<<"Enter Salary: ";
+	cin>>salary;
+	if(salary>50000)
+	{
+		cout<<"Salary is above than 50,000";
+	}
+	return 0;
+}
