a41d274135a66e3ac5bf3ca72fbe3359caa87fa6
shaguftamemon563-sketch <shaguftamemon563@gmail.com>
2026-07-13 07:57:51 -0700

    Add files via upload

---
+++ shagufta/if-else statement 5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num;
+	cout<<"Enter a Number: ";
+	cin>>num;
+	if(num%2==0)
+	{
+		cout<<"Number is Even";
+	}
+	else
+	{
+		cout<<"NOT Even";
+	}
+	return 0;
+}
