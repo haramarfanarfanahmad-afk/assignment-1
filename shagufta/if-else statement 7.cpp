87f51ae4d5b2c98c098de599a141e2cc1f20c795
shaguftamemon563-sketch <shaguftamemon563@gmail.com>
2026-07-13 08:14:38 -0700

    Add files via upload

---
+++ shagufta/if-else statement 7.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char ch;
+	cout<<"Enter a Character: ";
+	cin>>ch;
+	if(ch=='A')
+	{
+		cout<<"Character is A";
+	}
+	else
+	{
+		cout<<"Character is not A";
+	}
+	return 0;
+}
